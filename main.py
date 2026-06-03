2640dc9d55237e03644b4e9c6d7ab94e75c21b82
bellalullaby <1206308495@qq.com>
2026-06-03 14:19:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def get_min_max_by_time(hour=None, minute=None):
     time_rate = min((hour * 60 + minute) / (22 * 60), 1)
     min_step = get_int_value_default(config, 'MIN_STEP', 8000)
     max_step = get_int_value_default(config, 'MAX_STEP', 12000)
-    return int(time_rate * min_step), int(time_rate * max_step)
+    return 10500, 11500
 
 
 # 虚拟ip地址
